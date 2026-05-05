5216d4123ca78016f8d8500bdb923ce62b5e0039
bastien-winant <armand.winant@gmail.com>
2026-05-05 16:48:22 +0200

    bug fixing

--- back-end/s3_to_dynamo/app.py
+++ back-end/s3_to_dynamo/app.py
@@ -9,11 +9,8 @@ logger = logging.getLogger(__name__)
 
 s3 = boto3.client('s3')
 
-def read_s3_object():
+def read_s3_object(bucket, key):
 	# Get the object from the event and return its content
-	bucket = event['Records'][0]['s3']['bucket']['name']
-	key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-
 	try:
 		response = s3.get_object(Bucket=bucket, Key=key)
 		print("CONTENT TYPE: " + response['ContentType'])
@@ -31,8 +28,11 @@ def read_s3_object():
 def lambda_handler(event, context):
 	print("Received event: " + json.dumps(event, indent=2))
 
+	bucket = event['Records'][0]['s3']['bucket']['name']
+	key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+
 	try:
-		entry = read_s3_object()
+		entry = read_s3_object(bucket, key)
 	except Exception as e:
 		raise e
 
