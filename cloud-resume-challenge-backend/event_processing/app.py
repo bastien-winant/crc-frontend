97d4bb67dd6c55723d78d477b2d8e680f04bd43f
bastien-winant <armand.winant@gmail.com>
2026-05-07 00:15:46 +0200

    add try/except to event processing function

--- cloud-resume-challenge-backend/event_processing/app.py
+++ cloud-resume-challenge-backend/event_processing/app.py
@@ -6,28 +6,36 @@ from datetime import datetime
 # default encoding of bytes in the posted record
 ENCODING = 'utf-8'
 
-print('Loading function')
-
 def lambda_handler(event, context):
 	output = []
-
-	print("Received batch of {} records".format(len(event['records'])))
 
 	for record in event['records']:
 		payload = base64.b64decode(record['data']).decode(ENCODING)
 		event = json.loads(payload)
 
-		# TODO: apply transforms
-		trans_event = dict(event)
-		print("TRANSPORTED EVENT: ", trans_event)
+		try:
+			trans_event = {
+				'request_id': event['requestContext']['requestId'],
+				'request_datetime': event['requestContext']['requestTimeEpoch'],
+				'ip_address': event['requestContext']['identity'].get('sourceIp'),
+				'user_agent': event['headers'].get('User-Agent'),
+				'desktop': event['headers'].get('CloudFront-Is-Desktop-Viewer'),
+				'mobile': event['headers'].get('CloudFront-Is-Mobile-Viewer'),
+				'smart_tv': event['headers'].get('CloudFront-Is-SmartTV-Viewer'),
+				'tablet': event['headers'].get('CloudFront-Is-Tablet-Viewer'),
+				'country': event['headers'].get('CloudFront-Viewer-Country'),
+			}
 
-		trans_payload = json.dumps(trans_event) + "\n"
+			trans_payload = json.dumps(trans_event) + "\n"
 
-		output_record = {
-			'recordId': record['recordId'],
-			'result': 'Ok',
-			'data': base64.b64encode(trans_payload.encode(ENCODING)).decode(ENCODING)
-		}
-		output.append(output_record)
+			output_record = {
+				'recordId': record['recordId'],
+				'result': 'Ok',
+				'data': base64.b64encode(trans_payload.encode(ENCODING)).decode(ENCODING)
+			}
+			output.append(output_record)
+		except Exception as e:
+			logger.info(f"Failed to process event: {event}.")
+			raise e
 
 	return {'records': output}
