41ad4e6243211934f5d5a540d43b022367255369
bastien-winant <armand.winant@gmail.com>
2026-05-08 23:58:03 +0200

    apply enrichment

--- cloud-resume-challenge-backend/enrich_pipe_event/app.py
+++ cloud-resume-challenge-backend/enrich_pipe_event/app.py
@@ -17,8 +17,20 @@ def lambda_handler(event, context):
 			data = json.loads(payload)
 
 			# Do custom processing on the payload here
+			trans_data = {
+				'request_id': data['requestContext']['requestId'],
+				'request_datetime': data['requestContext']['requestTimeEpoch'],
+				'ip_address': data['requestContext']['identity'].get('sourceIp'),
+				'user_agent': data['headers'].get('User-Agent'),
+				'referer': data['headers'].get('Referer'),
+				'desktop': data['headers'].get('CloudFront-Is-Desktop-Viewer'),
+				'mobile': data['headers'].get('CloudFront-Is-Mobile-Viewer'),
+				'smart_tv': data['headers'].get('CloudFront-Is-SmartTV-Viewer'),
+				'tablet': data['headers'].get('CloudFront-Is-Tablet-Viewer'),
+				'country': data['headers'].get('CloudFront-Viewer-Country'),
+			}
 
-			record['data'] = base64.b64encode(json.dumps(data).encode('utf-8')).decode('utf-8')
+			record['data'] = base64.b64encode(json.dumps(trans_data).encode('utf-8')).decode('utf-8')
 			output.append(record)
 		except Exception as e:
 			logger.exception(f"An error occurred {e}")
