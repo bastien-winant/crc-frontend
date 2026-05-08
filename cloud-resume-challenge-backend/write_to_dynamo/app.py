f3241900ffcd3b332fe1ccfb8c0d019e1e7d8003
bastien-winant <armand.winant@gmail.com>
2026-05-08 12:10:58 +0200

    source/target lambda functions

--- cloud-resume-challenge-backend/write_to_dynamo/app.py
+++ cloud-resume-challenge-backend/write_to_dynamo/app.py
@@ -1,8 +1,7 @@
 import json
 import logging
 import boto3
-from botocore.exceptions import ClientError
-
+import base64
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -10,3 +9,14 @@ logger = logging.getLogger(__name__)
 
 def lambda_handler(event, context):
 	print("Received event: " + json.dumps(event, indent=2))
+
+	for record in event['Records']:
+		try:
+			print(f"Processed Kinesis Event - EventID: {record['eventID']}")
+			record_data = base64.b64decode(record['kinesis']['data']).decode('utf-8')
+			print(f"Record Data: {record_data}")
+		# TODO: Do interesting work based on the new data
+		except Exception as e:
+			print(f"An error occurred {e}")
+			raise e
+	print(f"Successfully processed {len(event['Records'])} records.")
